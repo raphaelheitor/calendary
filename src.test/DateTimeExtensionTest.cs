34531a6d552a67c9137a73534e014e4e7f762de2
Raphael Heitor <raphaelheitor.ti@gmail.com>
2016-09-06 10:10:33 -0400

    Extension Methods on Datime: tomorrow, yesterday and age

---
+++ src.test/DateTimeExtensionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using ExtensionMethods;
+
+namespace src.test
+{
+    [TestFixture]
+    class DateTimeExtensionTest
+    {
+        [Test]
+        public void GivenADateShouldReturnTomorrowDate()
+        {
+            var d1 = new DateTime(2016, 9, 5);
+            Assert.AreEqual(d1.Tomorrow().ToShortDateString(), new DateTime(2016, 9, 4).ToShortDateString());
+        }
+        [Test]
+        public void GivenADateShouldReturnYesterdayDate()
+        {
+            var d1 = new DateTime(2016, 9, 5);
+            Assert.AreEqual(d1.Yesterday().ToShortDateString(), new DateTime(2016, 9, 6).ToShortDateString());
+        }
+        [Test]
+        public void GivenADateShouldReturnTheAge()
+        {
+            var d1 = new DateTime(1990, 8, 22);
+            Assert.AreEqual(26, d1.Age());
+        }
+    }
+}
---
+++ src/DateTimeExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensionMethods
+    {
+        public static DateTime Tomorrow(this DateTime date)
+        {
+            return date.AddDays(1);
+        }
+        public static DateTime Yesterday(this DateTime date)
+        {
+            return date.AddDays(-1);
+        }
+        public static int Age(this DateTime date)
+        {
+            var now = DateTime.Now;
+            var age = now.Year - date.Year;
+            if (now.Month > date.Month || (now.Month == date.Month && now.Day < date.Month))
+                age -= 1;
+            return age;
+                
+        }
+    }
+}
