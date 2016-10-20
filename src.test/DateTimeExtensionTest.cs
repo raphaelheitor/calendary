09c64e12ff18f74385c47236313ec03f42570d31
Raphael Heitor <raphaelheitor.ti@gmail.com>
2016-10-20 16:43:42 -0400

    added new functions (checkers, between checker, next and last day of week)

--- src.test/DateTimeExtensionTest.cs
+++ src.test/DateTimeExtensionTest.cs
@@ -37,5 +37,303 @@ namespace src.test
             var d1 = DateTime.Now.AddDays(-15);
             Assert.AreEqual(15, d1.AgeInDays());
         }
+
+        [Test]
+        public void GivenADateInRangeShouldReturnTrue()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+            var first = new DateTime(2016, 10, 20);
+            var last = new DateTime(2016, 10, 25);
+            Assert.AreEqual(true, d1.Between(first, last));
+        }
+        [Test]
+        public void GivenADateOutOfRangeShouldReturnFalse()
+        {
+            var d1 = new DateTime(2016, 10, 19);
+            var first = new DateTime(2016, 10, 20);
+            var last = new DateTime(2016, 10, 25);
+            Assert.AreEqual(false, d1.Between(first, last));
+        }
+
+        [Test]
+        public void GivenADateShouldReturnTrueIfIsBirthdayDate()
+        {
+            var d1 = new DateTime(2000, 10, 21);
+            var birthday = new DateTime(2016, 10, 21);
+            Assert.AreEqual(true, d1.IsBirthday(birthday));
+        }
+        [Test]
+        public void GivenADateShouldReturnFalseIfNoBirthdayDate()
+        {
+            var d1 = new DateTime(2000, 10, 21);
+            var noBirthday = new DateTime(2016, 10, 11);
+            Assert.AreEqual(false, d1.IsBirthday(noBirthday));
+        }
+
+        [Test]
+        public void GivenAFutureDateShouldReturnTrue()
+        {
+            var d1 = new DateTime(2016, 10, 21);
+            Assert.AreEqual(true, d1.IsFuture());
+        }
+
+        [Test]
+        public void GivenAPastDateShouldReturnTrue()
+        {
+            var d1 = new DateTime(2016, 10, 19);
+            Assert.AreEqual(true, d1.IsPast());
+        }
+        [Test]
+        public void GivenAFutureDateShouldReturnFalse()
+        {
+            var d1 = new DateTime(2016, 10, 21);
+            Assert.AreEqual(false, d1.IsPast());
+        }
+
+        [Test]
+        public void GivenAPastDateShouldReturnFalse()
+        {
+            var d1 = new DateTime(2016, 10, 19);
+            Assert.AreEqual(false, d1.IsFuture());
+        }
+
+        [Test]
+        public void GivenADateWhenCheckDayofWeekShouldReturnExpectedDay()
+        {
+            var d1 = new DateTime(2016, 10, 21);
+            Assert.AreEqual(true, d1.IsFriday());
+        }
+        [Test]
+        public void GivenADateWhenCheckDayofWeekShouldReturnUnexpectedDay()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+            Assert.AreEqual(false, d1.IsFriday());
+        }
+
+        [Test]
+        public void GivenADateShoulRetornTheNextSunday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextSunday = new DateTime(2016, 10, 23);
+            Assert.AreEqual(nextSunday.Date, d1.NextSunday().Date);
+        }
+        [Test]
+        public void GivenASundayShoulRetornTheNextSunday()
+        {
+            var d1 = new DateTime(2016, 10, 16);
+
+            var nextSunday = new DateTime(2016, 10, 23);
+            Assert.AreEqual(nextSunday.Date, d1.NextSunday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheNextMonday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextMonday = new DateTime(2016, 10, 24);
+            Assert.AreEqual(nextMonday.Date, d1.NextMonday().Date);
+        }
+        [Test]
+        public void GivenAMondayShoulRetornTheNextMonday()
+        {
+            var d1 = new DateTime(2016, 10, 17);
+
+            var nextMonday = new DateTime(2016, 10, 24);
+            Assert.AreEqual(nextMonday.Date, d1.NextMonday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheNextTuesday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextTuesday = new DateTime(2016, 10, 25);
+            Assert.AreEqual(nextTuesday.Date, d1.NextTuesday().Date);
+        }
+        [Test]
+        public void GivenATuesdayShoulRetornTheNextTuesday()
+        {
+            var d1 = new DateTime(2016, 10, 18);
+
+            var nextTuesday = new DateTime(2016, 10, 25);
+            Assert.AreEqual(nextTuesday.Date, d1.NextTuesday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheNextWednesday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextWednesday = new DateTime(2016, 10, 26);
+            Assert.AreEqual(nextWednesday.Date, d1.NextWednesday().Date);
+        }
+        [Test]
+        public void GivenAWednesdayShoulRetornTheNextWednesday()
+        {
+            var d1 = new DateTime(2016, 10, 19);
+
+            var nextWednesday = new DateTime(2016, 10, 26);
+            Assert.AreEqual(nextWednesday.Date, d1.NextWednesday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheNextThursday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextThursday = new DateTime(2016, 10, 27);
+            Assert.AreEqual(nextThursday.Date, d1.NextThursday().Date);
+        }
+        [Test]
+        public void GivenAThursdayShoulRetornTheNextThursday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextThursday = new DateTime(2016, 10, 27);
+            Assert.AreEqual(nextThursday.Date, d1.NextThursday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheNextFriday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextFriday = new DateTime(2016, 10, 21);
+            Assert.AreEqual(nextFriday.Date, d1.NextFriday().Date);
+        }
+        [Test]
+        public void GivenAFridayShoulRetornTheNextFriday()
+        {
+            var d1 = new DateTime(2016, 10, 21);
+
+            var nextFriday = new DateTime(2016, 10, 28);
+            Assert.AreEqual(nextFriday.Date, d1.NextFriday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheNextSaturday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var nextSaturday = new DateTime(2016, 10, 22);
+            Assert.AreEqual(nextSaturday.Date, d1.NextSaturday().Date);
+        }
+        [Test]
+        public void GivenASaturdayShoulRetornTheNextSaturday()
+        {
+            var d1 = new DateTime(2016, 10, 22);
+
+            var nextSaturday = new DateTime(2016, 10, 29);
+            Assert.AreEqual(nextSaturday.Date, d1.NextSaturday().Date);
+        }
+
+        [Test]
+        public void GivenADateShoulRetornTheLastSunday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastSunday = new DateTime(2016, 10, 16);
+            Assert.AreEqual(lastSunday.Date, d1.LastSunday().Date);
+        }
+        [Test]
+        public void GivenASundayShoulRetornTheLastSunday()
+        {
+            var d1 = new DateTime(2016, 10, 23);
+
+            var nextSunday = new DateTime(2016, 10, 16);
+            Assert.AreEqual(nextSunday.Date, d1.LastSunday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheLastMonday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastMonday = new DateTime(2016, 10, 17);
+            Assert.AreEqual(lastMonday.Date, d1.LastMonday().Date);
+        }
+        [Test]
+        public void GivenAMondayShoulRetornTheLastMonday()
+        {
+            var d1 = new DateTime(2016, 10, 24);
+
+            var lastMonday = new DateTime(2016, 10, 17);
+            Assert.AreEqual(lastMonday.Date, d1.LastMonday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheLastTuesday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastTuesday = new DateTime(2016, 10, 18);
+            Assert.AreEqual(lastTuesday.Date, d1.LastTuesday().Date);
+        }
+        [Test]
+        public void GivenATuesdayShoulRetornTheLastTuesday()
+        {
+            var d1 = new DateTime(2016, 10, 25);
+
+            var lastTuesday = new DateTime(2016, 10, 18);
+            Assert.AreEqual(lastTuesday.Date, d1.LastTuesday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheLastWednesday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastWednesday = new DateTime(2016, 10, 19);
+            Assert.AreEqual(lastWednesday.Date, d1.LastWednesday().Date);
+        }
+        [Test]
+        public void GivenAWednesdayShoulRetornTheLastWednesday()
+        {
+            var d1 = new DateTime(2016, 10, 26);
+
+            var lastWednesday = new DateTime(2016, 10, 19);
+            Assert.AreEqual(lastWednesday.Date, d1.LastWednesday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheLastThursday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastThursday = new DateTime(2016, 10, 13);
+            Assert.AreEqual(lastThursday.Date, d1.LastThursday().Date);
+        }
+        [Test]
+        public void GivenAThursdayShoulRetornTheLastThursday()
+        {
+            var d1 = new DateTime(2016, 10, 27);
+
+            var lastThursday = new DateTime(2016, 10, 20);
+            Assert.AreEqual(lastThursday.Date, d1.LastThursday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheLastFriday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastFriday = new DateTime(2016, 10, 14);
+            Assert.AreEqual(lastFriday.Date, d1.LastFriday().Date);
+        }
+        [Test]
+        public void GivenAFridayShoulRetornTheLastFriday()
+        {
+            var d1 = new DateTime(2016, 10, 28);
+
+            var lastFriday = new DateTime(2016, 10, 21);
+            Assert.AreEqual(lastFriday.Date, d1.LastFriday().Date);
+        }
+        [Test]
+        public void GivenADateShoulRetornTheLastSaturday()
+        {
+            var d1 = new DateTime(2016, 10, 20);
+
+            var lastSaturday = new DateTime(2016, 10, 15);
+            Assert.AreEqual(lastSaturday.Date, d1.LastSaturday().Date);
+        }
+        [Test]
+        public void GivenASaturdayShoulRetornTheLastSaturday()
+        {
+            var d1 = new DateTime(2016, 10, 29);
+
+            var lastSaturday = new DateTime(2016, 10, 22);
+            Assert.AreEqual(lastSaturday.Date, d1.LastSaturday().Date);
+        }
     }
 }
--- src/DateTimeExtension.cs
+++ src/DateTimeExtension.cs
@@ -2,7 +2,7 @@
 
 namespace ExtensionMethods
 {
-    public static class MyExtensionMethods
+    public static class CalendaryExtensionMethods
     {
         public static DateTime Tomorrow(this DateTime date)
         {
@@ -25,6 +25,147 @@ namespace ExtensionMethods
         {
             TimeSpan ts = DateTime.Now - date;
             return ts.Duration().Days;
+        }
+
+        public static bool Between(this DateTime date, DateTime first, DateTime last)
+        {
+            return (date.Date >= first.Date && date.Date <= last.Date) ? true : false;
+        }
+
+        public static bool IsBirthday(this DateTime date, DateTime dateToCheck)
+        {
+            if (dateToCheck.Year <= date.Year)
+            {
+                return false;
+            }
+            return dateToCheck.Month == date.Month && dateToCheck.Day == date.Day;
+        }
+        public static bool IsBirthday(this DateTime date)
+        {
+            if (DateTime.Now.Year <= date.Year)
+            {
+                return false;
+            }
+            return DateTime.Now.Month == date.Month && DateTime.Now.Day == date.Day;
+        }
+
+        public static bool IsFuture(this DateTime date)
+        {
+            return (date.Date > DateTime.Now) ? true : false;
+        }
+        public static bool IsPast(this DateTime date)
+        {
+            return (date.Date < DateTime.Now) ? true : false;
+        }
+
+        public static bool IsSunday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Sunday)) ? true : false;
+        }
+        public static bool IsMonday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Monday)) ? true : false;
+        }
+        public static bool IsTuesday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Tuesday)) ? true : false;
+        }
+        public static bool IsWednesday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Wednesday)) ? true : false;
+        }
+        public static bool IsThursday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Thursday)) ? true : false;
+        }
+        public static bool IsFriday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Friday)) ? true : false;
+        }
+        public static bool IsSaturday(this DateTime date)
+        {
+            return (date.DayOfWeek.Equals(DayOfWeek.Saturday)) ? true : false;
+        }
+        public static DateTime NextSunday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Sunday);
+        }
+        public static DateTime NextMonday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Monday);
+        }
+        public static DateTime NextTuesday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Tuesday);
+        }
+        public static DateTime NextWednesday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Wednesday);
+        }
+        public static DateTime NextThursday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Thursday);
+        }
+        public static DateTime NextFriday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Friday);
+        }
+        public static DateTime NextSaturday(this DateTime date)
+        {
+            return CheckNextDayOfWeek(date, DayOfWeek.Saturday);
+        }
+
+        private static DateTime CheckNextDayOfWeek(DateTime date, DayOfWeek returnDay)
+        {
+            int i = 0;
+            while (!date.DayOfWeek.Equals(returnDay))
+            {
+                date = date.AddDays(1);
+                i++;
+            }
+
+            return (i == 0) ? date.AddDays(7) : date;
+        }
+
+        public static DateTime LastSunday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Sunday);
+        }
+        public static DateTime LastMonday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Monday);
+        }
+        public static DateTime LastTuesday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Tuesday);
+        }
+        public static DateTime LastWednesday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Wednesday);
+        }
+        public static DateTime LastThursday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Thursday);
+        }
+        public static DateTime LastFriday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Friday);
+        }
+        public static DateTime LastSaturday(this DateTime date)
+        {
+            return CheckLastDayOfWeek(date, DayOfWeek.Saturday);
+        }
+
+        private static DateTime CheckLastDayOfWeek(DateTime date, DayOfWeek returnDay)
+        {
+            int i = 0;
+            while (!date.DayOfWeek.Equals(returnDay))
+            {
+                date = date.AddDays(-1);
+                i++;
+            }
+
+            return (i == 0) ? date.AddDays(-7) : date;
         }
     }
 }
